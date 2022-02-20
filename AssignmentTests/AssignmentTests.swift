2f64360a8a2ab740ce208d89d6ca22dbdab883b2
Toan <Trungtoan1093@gmail.com>
2022-02-20 17:10:52 +0700

    update unit test

--- AssignmentTests/AssignmentTests.swift
+++ AssignmentTests/AssignmentTests.swift
@@ -10,6 +10,7 @@ import XCTest
 
 class AssignmentTests: XCTestCase {
 
+    let cityTest: CityModel = CityModel(country: "US", name: "Alabama", _id: 4829764, coord: CoordinateModel(lon: -86.750259, lat: 32.750408))
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -24,6 +25,16 @@ class AssignmentTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        SearchService.shared.loadData { status in
+            let searchService: SearchCityProtocol = SearchDefaultImplement(cities: SearchService.shared.cities)
+            self.testCityName(searchText: "Ala", searchService: searchService)
+            self.testCityNameParticular(searchText: "Alabama", searchService: searchService)
+            self.testNameCityFail(searchText: "Alabamaaaa", searchService: searchService)
+            self.testCounttry(searchText: "US", searchService: searchService)
+            self.testCounttryFail(searchText: "USsss", searchService: searchService)
+            
+        }
     }
 
     func testPerformanceExample() throws {
@@ -33,16 +44,36 @@ class AssignmentTests: XCTestCase {
         }
     }
     
-    func testSearch() {
-        SearchService.shared.loadData { status in
-            print(status)
-            let citis: [CityModel] = SearchService.shared.cities
-            let seachSeviec: SearchCityProtocol = SearchDefaultImplement(cities: citis)
-            seachSeviec.search(text: "AU") { cities in
-                XCTAssert(cities.count > 0, "")
-            }
+    func testCityName(searchText: String, searchService: SearchCityProtocol) {
+        searchService.search(text: searchText) { cities in
+            XCTAssert(cities.count > 1)
         }
-        
+    }
+
+    func testCityNameParticular(searchText: String, searchService: SearchCityProtocol) {
+        searchService.search(text: searchText) { cities in
+            XCTAssert(cities.count == 1)
+            XCTAssertEqual(self.cityTest.name, cities.first?.name)
+            XCTAssertEqual(self.cityTest._id, cities.first?._id)
+        }
+    }
+    
+    func testNameCityFail(searchText: String, searchService: SearchCityProtocol) {
+        searchService.search(text: searchText) { cities in
+            XCTAssert(cities.count > 0)
+        }
+    }
+    
+    func testCounttry(searchText: String, searchService: SearchCityProtocol) {
+        searchService.search(text: searchText) { cities in
+            XCTAssert(cities.count > 0)
+        }
+    }
+    
+    func testCounttryFail(searchText: String, searchService: SearchCityProtocol) {
+        searchService.search(text: searchText) { cities in
+            XCTAssert(cities.count > 0)
+        }
     }
 
 }
